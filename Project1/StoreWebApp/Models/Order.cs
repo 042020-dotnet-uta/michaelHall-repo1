edc02a9192d2c04eebc1f6865f2020cd51bbc2fc
Mibyhall <mhall1914@comcast.net>
2020-05-19 01:42:46 -0400

    rearranges testing and adds order/store scaffolds

--- Project1/StoreWebApp/Models/Order.cs
+++ Project1/StoreWebApp/Models/Order.cs
@@ -3,17 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace StoreWebApp.Models
 {
     public class Order
     {
         public int Id { get; set; }
+
+        [Display(Name = "Product")]
         public int ProductId { get; set; }
         public Product Product { get; set; }
+
+        [Display(Name = "Customer")]
         public int CustomerId { get; set; }
         public Customer Customer { get; set; }
         public int Quantity { get; set; }
-        public DateTime Timestamp { get; set; }
+        private DateTime timestamp;
+        public DateTime Timestamp
+        {
+            get { return timestamp; }
+            set { timestamp = DateTime.Now; }
+        }
     }
 }
